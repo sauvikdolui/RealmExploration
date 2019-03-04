edebee51435d41bd9328761f1465656f66d6e15a
Sauvik Dolui <sauvik.dolui@effectives-macbook-pro.local>
2019-03-04 09:59:03 +0530

    Initial Commit

---
+++ RealmExploration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmExploration
+//
+//  Created by Sauvik Dolui on 04/03/19.
+//  Copyright © 2019 Sauvik Dolui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
