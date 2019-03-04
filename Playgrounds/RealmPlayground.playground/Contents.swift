760fb88461d9ec488d8dab744c17fd2b9418ba62
Sauvik Dolui <sauvik.dolui@Effectives-MacBook-Pro.local>
2019-03-04 11:19:34 +0530

    Initial Push

---
+++ Playgrounds/RealmPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+import XCPlayground
+import PlaygroundSupport
+import RealmSwift
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+
+// Define your models like regular Swift classes
+class Dog: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+}
+class Person: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var picture: Data? = nil // optionals supported
+    let dogs = List<Dog>()
+}
+
+// Use them like regular Swift objects
+let myDog = Dog()
+myDog.name = "Rex"
+myDog.age = 1
+print("name of dog: \(myDog.name)")
+
+// Get the default Realm
+let realm = try! Realm()
+
+// Query Realm for all dogs less than 2 years old
+let puppies = realm.objects(Dog.self).filter("age < 2")
+puppies.count // => 0 because no dogs have been added to the Realm yet
+
+// Persist your data easily
+try! realm.write {
+    realm.add(myDog)
+}
+
+// Queries are updated in realtime
+puppies.count // => 1
+
+// Query and update from any thread
+DispatchQueue(label: "background").async {
+    autoreleasepool {
+        let realm = try! Realm()
+        let theDog = realm.objects(Dog.self).filter("age == 1").first
+        try! realm.write {
+            theDog!.age = 3
+        }
+    }
+}
+
